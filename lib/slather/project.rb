dd82df156bc4f09128fa31a855865968255900e2
Nano <nano.anderson@gmail.com>
2015-12-11 11:46:16 -0500

    Update project.rb

--- lib/slather/project.rb
+++ lib/slather/project.rb
@@ -144,7 +144,7 @@ module Slather
       if profdata_coverage_dir == nil || (coverage_profdata = Dir["#{profdata_coverage_dir}/**/Coverage.profdata"].first) == nil
         raise StandardError, "No Coverage.profdata files found. Please make sure the \"Code Coverage\" checkbox is enabled in your scheme's Test action or the build_directory property is set."
       end
-      llvm_cov_args = %W(show -arch i386 -instr-profile #{coverage_profdata} #{binary_file})
+      llvm_cov_args = %W(show -instr-profile #{coverage_profdata} #{binary_file})
       puts "Running 'xcrun llvm-cov #{llvm_cov_args.shelljoin}'"
       `xcrun llvm-cov #{llvm_cov_args.shelljoin}`
     end
